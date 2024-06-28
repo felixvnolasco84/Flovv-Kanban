bb816d25edb3166b4ab3ee76b932f592fb3dd079
felixvnolasco84 <felix@polygonag.com>
2024-06-28 13:46:05 -0600

    :tada:

---
+++ components/logo.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
+        {/* <Image
+          src="/logo.svg"
+          alt="Logo"
+          height={30}
+          width={30}
+        /> */}
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          FLOVV
+        </p>
+      </div>
+    </Link>
+  );
+};
